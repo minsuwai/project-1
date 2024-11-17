c0f8fc50558110fa196bf6201ded0d9447f9d56d
minsuwai <mswwai15@gmail.com>
2024-11-17 14:34:55 +0630

    feat: updated services section

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         primary: "#FEFFF0",
         secondary: "#0C0C0C",
         accent: "#ff6347",
+        button: "#BAE6FF",
       },
       fontFamily: {
         primary: ["Space Grotesk"],
